2083d1480494a8b1219a6c16afeba64664fe90e1
sumaeta <brenosumaeta33@gmail.com>
2021-03-31 16:36:50 -0300

    Implementando a busca por Id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFabrica;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Program {
@@ -10,14 +13,19 @@ public class Program {
 	
 		VendedorDao vendedorDao = DaoFabrica.criarVendedorDao();
 		
-		System.out.println("=== teste === ");
+		System.out.println("=== teste ===");
 		
 		Vendedor vendedor = vendedorDao.findById(3);
 		
 		System.out.println(vendedor);
 		
 		
-		
+		System.out.println("=== teste 2 ===");
+		Departamento departamento = new Departamento(2, null);
+		List<Vendedor> list = vendedorDao.findByDepartamento(departamento);
+		for (Vendedor obj : list) {
+			System.out.println(obj);
+		}
 		
 		
 	}
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -91,6 +94,45 @@ public class VendedorDaoJDBC implements VendedorDao {
 	public List<Vendedor> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Vendedor> findByDepartamento(Departamento departamento) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"select vendedor. *, departamento.Nome as depName " +
+					"from vendedor inner join departamento " + 
+					"on vendedor.DepartamentoId = departamento.Id " +
+					"where DepartamentoId = ? " +
+					"order by Nome ");
+			
+			st.setInt(1, departamento.getId());
+			rs = st.executeQuery();
+			List<Vendedor> list = new ArrayList<>();
+			
+			Map<Integer, Departamento> map = new HashMap<>();
+			
+			while (rs.next()) {
+				
+				Departamento dep = map.get(rs.getInt("DepartamentoId"));
+				
+				if(dep == null) {
+				dep = instantiateDepartamento(rs);
+				map.put(rs.getInt("DepartamentoId"), dep);
+				}
+				Vendedor obj = instantiateVendedor(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatemant(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
