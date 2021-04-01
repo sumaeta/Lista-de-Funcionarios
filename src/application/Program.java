cb33e3ed72ab783128c450e52c37c3003936c3cb
sumaeta <brenosumaeta33@gmail.com>
2021-04-01 11:48:34 -0300

    Inserindo Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFabrica;
@@ -13,14 +14,14 @@ public class Program {
 	
 		VendedorDao vendedorDao = DaoFabrica.criarVendedorDao();
 		
-		System.out.println("=== teste ===");
+		System.out.println("=== teste Filtrando por id ===");
 		
 		Vendedor vendedor = vendedorDao.findById(3);
 		
 		System.out.println(vendedor);
 		
 		
-		System.out.println("=== teste 2 ===");
+		System.out.println("=== teste 2 Filtrando por Departamento===");
 		Departamento departamento = new Departamento(2, null);
 		List<Vendedor> list = vendedorDao.findByDepartamento(departamento);
 		for (Vendedor obj : list) {
@@ -28,11 +29,16 @@ public class Program {
 		}
 		
 		
-		System.out.println("=== teste 3 ===");
+		System.out.println("=== teste 3 retirando o filtro===");
 		list = vendedorDao.findAll();
 		for (Vendedor obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== teste 3 inserindo vendedor ===");
+		Vendedor newVendedor = new Vendedor(null, "Greg", "Greg@gmail.com", new Date(), 4.000, departamento);
+		vendedorDao.insert(newVendedor);
+		System.out.println("Inserindo novo id: " + newVendedor.getId());
 		
 	}
 
